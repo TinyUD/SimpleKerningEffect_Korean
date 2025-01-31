c3fd5030a6473953af8d89162965fb08480918e0
TinyUD <55289490+TinyUD@users.noreply.github.com>
2025-01-31 23:42:38 +0900

    한국어 버전 수정

--- SimpleKerningEffect/Effects/SimpleKerningEffect.cs
+++ SimpleKerningEffect/Effects/SimpleKerningEffect.cs
@@ -7,58 +7,58 @@ using YukkuriMovieMaker.Plugin.Effects;
 
 namespace SimpleKerningEffect.Effects
 {
-    [VideoEffect("簡易カーニング", ["テキスト"], ["kerning", "文字", "テキスト", "text", "プラグイン", "plugin"], isAviUtlSupported:false, isEffectItemSupported:false)]
+    [VideoEffect("간단 커닝", ["텍스트"], ["kerning", "문자", "텍스트", "text", "플러그인", "plugin"], isAviUtlSupported: false, isEffectItemSupported: false)]
     internal class SimpleKerningEffect : VideoEffectBase
     {
-        public override string Label => "簡易カーニング";
+        public override string Label => "간단 커닝";
 
-        [Display(GroupName = "簡易カーニング\r\n対象の文字位置を数字で指定します\r\nカンマで複数指定、ハイフンで範囲指定", Name = "文字位置", Description = "何文字目を対象にするか設定します\r\n例：1,3,5-10")]
+        [Display(GroupName = "간단 커닝\r\n대상 문자 위치를 숫자로 지정합니다\r\n쉼표(,)로 여러 개 지정, 하이픈(-)으로 범위 지정", Name = "문자 위치", Description = "몇 번째 문자를 대상으로 할지 설정합니다\r\n예: 1,3,5-10")]
         [TextEditor(AcceptsReturn = true)]
         public string Index { get => index; set => Set(ref index, value); }
         string index = string.Empty;
 
-        [Display(GroupName = "描画", Name = "X", Description = "描画位置（横方向）")]
+        [Display(GroupName = "그리기", Name = "X", Description = "그리기 위치(가로 방향)")]
         [AnimationSlider("F1", "px", -500, 500)]
         public Animation X { get; } = new Animation(0, -99999, 99999);
 
-        [Display(GroupName = "描画", Name = "Y", Description = "描画位置（縦方向）")]
+        [Display(GroupName = "그리기", Name = "Y", Description = "그리기 위치(세로 방향)")]
         [AnimationSlider("F1", "px", -500, 500)]
         public Animation Y { get; } = new Animation(0, -99999, 99999);
 
-        [Display(GroupName = "描画", Name = "Z", Description = "描画位置（奥行き）")]
+        [Display(GroupName = "그리기", Name = "Z", Description = "그리기 위치(깊이)")]
         [AnimationSlider("F1", "px", -500, 500)]
         public Animation Z { get; } = new Animation(0, -99999, 99999);
 
-        [Display(GroupName = "描画", Name = "不透明度", Description = "不透明度")]
+        [Display(GroupName = "그리기", Name = "불투명도", Description = "불투명도")]
         [AnimationSlider("F1", "%", 0, 100)]
         public Animation Opacity { get; } = new Animation(100, 0, 100);
 
-        [Display(GroupName = "描画", Name = "左右反転", Description = "左右反転")]
+        [Display(GroupName = "그리기", Name = "좌우 반전", Description = "좌우 반전")]
         [ToggleSlider]
         public bool Invert { get => invert; set => Set(ref invert, value); }
         bool invert = false;
 
-        [Display(GroupName = "拡大率", Name = "全体", Description = "全体の拡大率")]
+        [Display(GroupName = "확대율", Name = "전체", Description = "전체 확대율")]
         [AnimationSlider("F1", "%", 0, 500)]
         public Animation Zoom { get; } = new Animation(100, 0, 5000);
 
-        [Display(GroupName = "拡大率", Name = "横方向", Description = "横方向の拡大率")]
+        [Display(GroupName = "확대율", Name = "가로 방향", Description = "가로 방향 확대율")]
         [AnimationSlider("F1", "%", 0, 500)]
         public Animation ZoomX { get; } = new Animation(100, 0, 5000);
 
-        [Display(GroupName = "拡大率", Name = "縦方向", Description = "縦方向の拡大率")]
+        [Display(GroupName = "확대율", Name = "세로 방향", Description = "세로 방향 확대율")]
         [AnimationSlider("F1", "%", 0, 500)]
         public Animation ZoomY { get; } = new Animation(100, 0, 5000);
 
-        [Display(GroupName = "回転角", Name = "X軸", Description = "横方向、X軸に対する回転角")]
+        [Display(GroupName = "회전 각도", Name = "X축", Description = "가로 방향, X축에 대한 회전 각도")]
         [AnimationSlider("F1", "°", -360, 360)]
         public Animation RotationX { get; } = new Animation(0, -36000, 36000);
 
-        [Display(GroupName = "回転角", Name = "Y軸", Description = "縦方向、Y軸に対する回転角")]
+        [Display(GroupName = "회전 각도", Name = "Y축", Description = "세로 방향, Y축에 대한 회전 각도")]
         [AnimationSlider("F1", "°", -360, 360)]
         public Animation RotationY { get; } = new Animation(0, -36000, 36000);
 
-        [Display(GroupName = "回転角", Name = "Z軸", Description = "平面方向、Z軸に対する回転角")]
+        [Display(GroupName = "회전 각도", Name = "Z축", Description = "평면 방향, Z축에 대한 회전 각도")]
         [AnimationSlider("F1", "°", -360, 360)]
         public Animation RotationZ { get; } = new Animation(0, -36000, 36000);
 
